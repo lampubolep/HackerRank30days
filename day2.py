9a965978e516f7a0ed5f1c5f122ac4cb656166df
lampubolep <chieldesickn@gmail.com>
2019-11-15 13:00:17 +0700

    Round .5 up

--- day2.py
+++ day2.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 meal_cost = float(input())
 tip_percent = int(input())
 tax_percent = int(input())
@@ -7,6 +9,6 @@ tip = meal_cost*(tip_percent/100)
 
 tax = meal_cost*(tax_percent/100)
 
-totalCost = round(meal_cost + tip + tax)
+totalCost = Decimal(meal_cost + tip + tax).quantize(Decimal('1'))
 
 print(totalCost)
