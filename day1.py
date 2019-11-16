bc35a878c2a628443d353611befe3b20658f1075
lampubolep <chieldesickn@gmail.com>
2019-11-16 11:02:48 +0700

    Day 1: add comments

--- day1.py
+++ day1.py
@@ -1,11 +1,14 @@
+# Define variable
 i = 4
 d = 4.0
 s = 'HackerRank '
 
+# Receiving input
 integer = int( input() )
 double = float( input() )
 string = input()
 
+# Print output (1. Sum integer 2. Sum float 3. Concat string)
 print(i+integer)
 print(d+double)
 print(s+string)
