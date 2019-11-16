3a4ef17c09c50b2fb4ee5e013dd4b3efc3dd2cc0
lampubolep <chieldesickn@gmail.com>
2019-11-16 11:01:14 +0700

    Day 2: add comments

--- day2.py
+++ day2.py
@@ -1,14 +1,18 @@
 from decimal import Decimal
 
+# input meal cost, tip percent and tax percent
 meal_cost = float(input())
 tip_percent = int(input())
 tax_percent = int(input())
-#solve(meal_cost, tip_percent, tax_percent)
 
+# tip formula
 tip = meal_cost*(tip_percent/100)
 
+# tax formula
 tax = meal_cost*(tax_percent/100)
 
+# round .5 up
 totalCost = Decimal(meal_cost + tip + tax).quantize(Decimal('1'))
 
+# print total Cost
 print(totalCost)
